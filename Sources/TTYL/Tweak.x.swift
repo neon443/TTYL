a4fd4e5c40c46dc8f6712ce71e2981ca3510ccdd
neon443 <69979447+neon443@users.noreply.github.com>
2025-10-02 17:51:45 +0100

    YES FINALLY YESYEYSYES LFGGGG hooked the dialer ui!!!!!!!!!!!1

--- Sources/TTYL/Tweak.x.swift
+++ Sources/TTYL/Tweak.x.swift
@@ -2,11 +2,27 @@ import Foundation
 import Orion
 import TTYLC
 import UIKit
-import CallKit
 import AudioToolbox
 
-class TTYHook: ClassHook<CXCallObserver> {
-	func setText(_ text: String) {
-		orig.setText("sybau 🥀")
+class xhook: ClassHook<UIViewController> {
+	static var targetName: String { "PHInCallKeypadViewController" }
+	
+	func viewDidLoad() {
+		orig.viewDidLoad()
+		
+		DispatchQueue.main.asyncAfter(deadline: .now()+1) {
+			let l = UILabel()
+			l.textColor = .black
+			l.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+			l.text = ""
+			self.target.view.addSubview(l)
+			self.target.view.bringSubviewToFront(l)
+		}
+		
+		AudioServicesPlayAlertSoundWithCompletion(1103) {
+			AudioServicesPlayAlertSoundWithCompletion(1103) {
+				AudioServicesPlayAlertSound(1103)
+			}
+		}
 	}
 }
